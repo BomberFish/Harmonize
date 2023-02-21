092e2fa8aea1e888edca31779118ac313eb3a231
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-02-21 16:36:21 -0500

    Initial Commit

---
+++ Harmonize/HarmonizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarmonizeApp.swift
+//  Harmonize
+//
+//  Created by Hariz Shirazi on 2023-02-21.
+//
+
+import SwiftUI
+
+@main
+struct HarmonizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
